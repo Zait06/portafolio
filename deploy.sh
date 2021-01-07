e6e05ca8be9a9b4404c3bcdf2a1eb896094621a2
Zait06 <zait-hdez@hotmail.com>
2021-01-06 20:07:00 -0600

    deploy tools

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:Zait06/Zait06.github.io.git master
+# git push -f git@github.com:Zait06/Zait06.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:Zait06/portafolio.git master:gh-pages
 
 cd -
