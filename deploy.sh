cc4570ca19a3c5274fe17ee5d91263c6596b8233
Zait06 <zait-hdez@hotmail.com>
2021-01-06 21:56:24 -0600

    Docs

--- deploy.sh
+++ deploy.sh
@@ -4,23 +4,24 @@
 set -e
 
 # build
-mv build docs
 npm run build
 
 # navigate into the build output directory
-cd dist
+mv dist docs
+cd docs
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 git init
-git add -A
+git add .
 git commit -m 'deploy'
+git branch -M master
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:Zait06/Zait06.github.io.git master
+git push -f git@github.com:Zait06/Zait06.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Zait06/portafolio.git master:gh-pages
+#git push -f git@github.com:Zait06/portafolio.git master:gh-pages
 
 cd -
