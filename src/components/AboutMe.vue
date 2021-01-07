ef159ea1ad103b41c7fcf989f05c4d30cdf0758f
Zait06 <zait-hdez@hotmail.com>
2021-01-07 12:20:53 -0600

    deploy tools

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -5,7 +5,7 @@
                 <p class="title has-text-white" data-aos="fade-right">About me</p>
                 <p class="has-text-justified my-text" data-aos="fade-right">
                     I'm a computer-systems engineer who likes to learn new things and face new challenges.
-                    I have knowledge about different some programing language, frameworks frontend and database manager.
+                    I have knowledge about some programing language, frameworks frontend and database manager.
                     <br>
                     Talking about something more personal... I love dancing, doing workout, and going out with friends.
                 </p>
